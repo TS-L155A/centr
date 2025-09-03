4daa0d539e807302a29195256cf249708367b1f5
ultraviolation <tsaprov@gmail.com>
2025-09-03 20:51:06 +0500

    стартовый дудос

---
+++ src/main/java/com/ts2/centr/CentrApplication.java
@@ -0,0 +1,13 @@
+package com.ts2.centr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ts2/centr/CentrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts2.centr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
