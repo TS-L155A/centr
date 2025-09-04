754ff42c961ba32f8f89b7d966e3e1c8716e7cdf
ultraviolation <tsaprov@gmail.com>
2025-09-04 14:07:38 +0500

    Создал DB и сделал красивый вывод на сайт таблицы через посты

---
+++ src/main/java/com/ts2/centr/repo/PostRepository.java
@@ -0,0 +1,9 @@
+package com.ts2.centr.repo;
+
+import com.ts2.centr.models.Post;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
