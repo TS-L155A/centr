fdaa71b4fcb507e9abe408629d4f73ff1713d8fe
ultraviolation <tsaprov@gmail.com>
2025-09-21 08:53:42 +0500

    Перепроверка бага и чистка страниц

--- src/main/java/com/ts2/centr/controllers/HomeController.java
+++ src/main/java/com/ts2/centr/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.ts2.centr.controllers;
 
+import com.ts2.centr.models.Post;
+import com.ts2.centr.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/")
     public String greeting(Model model) {
         model.addAttribute("title", "Главная еблица");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
 
