2884d1552d4a0b7058f9dcb902222b0bc37de526
CORE <tsaprov@gmail.com>
2025-12-09 19:11:25 +0500

    Сервис обработки юзеров

---
+++ src/main/java/com/ts2/centr/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.ts2.centr.security;
+
+import com.ts2.centr.repo.UserRepository;
+import com.ts2.centr.security.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("ЭЭЭ ебать ты кто такой? че за " + username));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
