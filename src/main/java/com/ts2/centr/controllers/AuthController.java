431c634c2264882de6d1c9af44148408a6d44e6d
CORE <tsaprov@gmail.com>
2025-12-13 21:17:36 +0500

    ЛОГИН ЕНД

--- src/main/java/com/ts2/centr/controllers/AuthController.java
+++ src/main/java/com/ts2/centr/controllers/AuthController.java
@@ -1,14 +1,46 @@
 package com.ts2.centr.controllers;
 
+import com.ts2.centr.security.Role;
+import com.ts2.centr.security.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
 
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
     public String loginPage() {
         return "login"; // login.html
+    }
+
+    @GetMapping("/register")
+    public String regPage() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(
+            @RequestParam String username,
+            @RequestParam String password,
+            @RequestParam Role role,
+            Model model
+            ) {
+        try {
+            userService.register(username, password, role);
+            return "redirect:/login";
+        } catch (Exception e) {
+            model.addAttribute("error", "Пользователь еблан");
+            return "register";
+        }
     }
 
 }
