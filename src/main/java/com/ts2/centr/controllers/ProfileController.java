b16fe8bb7234f9fbb8b6d539272c3b2a46509416
CORE <tsaprov@gmail.com>
2025-12-16 15:37:03 +0500

    Личный кабинет (Профиль) Стартовая реализация, юзнейм + роль

--- src/main/java/com/ts2/centr/controllers/ProfileController.java
+++ src/main/java/com/ts2/centr/controllers/ProfileController.java
@@ -1,4 +1,27 @@
 package com.ts2.centr.controllers;
 
+import com.ts2.centr.repo.UserRepository;
+import com.ts2.centr.security.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProfileController {
+    private final UserRepository userRepository;
+
+    public ProfileController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/profile")
+    public String profile(Authentication authentication, Model model){
+
+        String username = authentication.getName();
+        User user = userRepository.findByUsername(username).orElseThrow();
+
+        model.addAttribute("user", user);
+        return "profile";
+    }
 }
